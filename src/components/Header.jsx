f5c3d80312fcbfc7240b04dd7b1b9028e7f648a0
Somayeh <somayehkargosha@gmail.com>
2024-05-12 22:41:26 +0330

    Update Search button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,32 +9,49 @@ import { useState } from "react";
 
 function Header() {
   const [openDropDownNav, setOpenDropDownNav] = useState(false);
+  // const [selected, setSelected] = useState(false);
   const className =
     "hover:border-b-2 hover:border-gray-700 opacity-60 flex flex-col items-center hover:opacity-100";
 
   return (
     <>
-      <header className="sticky z-10 top-0 container max-w-full py-[2px] bg-white border-b">
-        <div className="container lg:max-w-full px-2 md:max-w-4xl sm:max-w-3xl md:py-0 py-2">
-          <nav className="flex items-center justify-between lg:justify-around xl:justify-evenly ">
-            <div className="flex">
-              <a href="">
-                <img
-                  src="public/Linkedin_logo_initials.png"
-                  alt=""
-                  className="md:w-[34px] md:h-[34px] w-7 h-7"
-                />
-              </a>
-              <div className="relative">
-                <MdOutlineSearch className="absolute md:left-4 left-1 lg:top-2 lg:text-lg md:text-4xl text-2xl top-[2px] md:top-0" />
+      <header className="sticky z-10 top-0 md:py-[2px] py-[7px] container md:px-[8px] px-1 max-w-full bg-white border-b">
+        <div className="container md:max-w-6xl max-w-[580px] mx-auto flex items-center lg:justify-between md:justify-around justify-between space-x-2">
+          <div className="max-w-[480px] flex space-x-2 items-center">
+            <a href="">
+              <img
+                src="public/Linkedin_logo_initials.png"
+                alt=""
+                className="md:w-[38px] md:h-[38px] w-7 h-7"
+              />
+            </a>
+            <div className="max-w-[280px] relative">
+              <div className="md:hidden lg:block hidden">
                 <input
                   type="text"
                   name="search"
                   placeholder="Search"
-                  className="bg-blue-100 p-[5px] ml-1 w-72 rounded placeholder:text-right lg:block text-right hidden lg:placeholder:mr-10 placeholder:text-sm"
+                  className="py-[6px] px-[42px] bg-blue-100 rounded placeholder:text-right text-right placeholder:text-sm"
                 />
+                <MdOutlineSearch className="absolute text-xl top-2 left-4" />
               </div>
+              <button className="lg:hidden flex flex-col items-center font-bold opacity-70">
+                <MdOutlineSearch className="md:text-2xl text-3xl" />
+                <div className="text-[10px] pl-2 hidden md:block">Search</div>
+              </button>
             </div>
+          </div>
+          {/* <div className="relative">
+            <MdOutlineSearch className="absolute md:left-4 left-1 lg:top-2 lg:text-lg md:text-4xl text-2xl top-[2px] md:top-0" />
+
+            <input
+              type="text"
+              name="search"
+              placeholder="Search"
+              className={`bg-blue-100 p-[5px] ml-1 w-72 rounded placeholder:text-right lg:block text-right lg:placeholder:mr-10 placeholder:text-sm`}
+            />
+          </div> */}
+          <nav>
             <ul className="items-center flex md:space-x-6 space-x-[18px] text-[10px] font-semibold">
               <li>
                 <a className={className} href="#">
